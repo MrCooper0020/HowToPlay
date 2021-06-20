ecf5d40879cdb4453a89c0260cb5445f1771d311
Matheus H. Potrich <matheushp.com@hotmail.com>
2021-06-20 13:46:05 -0300

    TipsRoom styles and adjusts

--- src/views/TipsRoom.js
+++ src/views/TipsRoom.js
@@ -1,22 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import { Card, ListItem, Button, Icon } from "react-native-elements";
 
 import * as TipsAction from "../services/actions/tipsAction";
 import * as TipAction from "../services/actions/tipAction";
 import * as UsersAction from "../services/actions/usersAction";
 import { useSelector, useDispatch } from "react-redux";
+import mainStyle from "../Styles/main";
 
 export default function TipsRoom({ route, navigation }) {
     const dispatch = useDispatch();
     const { game } = route.params;
+    const login = useSelector((store) => store.login);
     const tips = useSelector((store) => store.tips);
     const users = useSelector((store) => store.users);
+    const [currentUser, setCurrentUser] = useState(false);
 
     useLayoutEffect(() => {
-        dispatch(TipsAction.getAll());
-        dispatch(UsersAction.getAll());
+        async function loadData() {
+            try {
+                await dispatch(TipsAction.getAll());
+                await dispatch(UsersAction.getAll());
+
+                // save the id of current user
+                if (login.email) {
+                    users.forEach((user) => {
+                        if (login.email == user.email) {
+                            setCurrentUser(user);
+                        }
+                    });
+                }
+            } catch (error) {}
+        }
+        loadData();
     }, [dispatch]);
 
     async function deleteTip(tipId) {
@@ -30,53 +47,112 @@ export default function TipsRoom({ route, navigation }) {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
-            <Button
-                title="Adicionar nova dica"
-                onPress={() => navigation.navigate("NewTip", { game })}
-            />
-            {tips.map((tip, i) => {
-                if (game.id == tip.gameId) {
-                    let currentUser;
+            <SafeAreaView
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                <ScrollView style={{ paddingTop: 20 }}>
+                    <Button
+                        title="Adicionar nova dica"
+                        buttonStyle={mainStyle.normalButton}
+                        onPress={() => navigation.navigate("NewTip", { game })}
+                    />
+                    {tips.map((tip, i) => {
+                        if (game.id == tip.gameId) {
+                            let tipCreator;
 
-                    users.forEach((user) => {
-                        if (user.id == tip.userId) {
-                            currentUser = user;
+                            console.log(currentUser);
+
+                            users.forEach((user) => {
+                                if (user.id == tip.userId) {
+                                    tipCreator = user;
+                                }
+                            });
+
+                            return (
+                                <Card
+                                    key={i}
+                                    containerStyle={{
+                                        marginHorizontal: 0,
+                                        borderRadius: 10,
+                                    }}
+                                >
+                                    <Card.Title>{tipCreator.name}</Card.Title>
+                                    <Card.Divider />
+                                    <Text
+                                        style={{
+                                            marginBottom: 15,
+                                            marginHorizontal: 10,
+                                        }}
+                                    >
+                                        {tip.tip}
+                                    </Text>
+                                    {currentUser.id == tip.userId ? (
+                                        <>
+                                            <Card.Divider />
+                                            <View
+                                                style={{
+                                                    flexDirection: "row",
+                                                    justifyContent:
+                                                        "space-around",
+                                                    width: "100%",
+                                                }}
+                                            >
+                                                <Button
+                                                    title="Editar"
+                                                    buttonStyle={
+                                                        styles.commentButtonEdit
+                                                    }
+                                                    containerStyle={{
+                                                        width: "40%",
+                                                    }}
+                                                    onPress={() =>
+                                                        navigation.navigate(
+                                                            "NewTip",
+                                                            {
+                                                                game,
+                                                                tip,
+                                                            }
+                                                        )
+                                                    }
+                                                />
+                                                <Button
+                                                    title="Apagar"
+                                                    buttonStyle={
+                                                        styles.commentButtonDelete
+                                                    }
+                                                    containerStyle={{
+                                                        width: "40%",
+                                                    }}
+                                                    onPress={() =>
+                                                        deleteTip(tip.id)
+                                                    }
+                                                />
+                                            </View>
+                                        </>
+                                    ) : null}
+                                </Card>
+                            );
                         }
-                    });
-
-                    return (
-                        <Card key={i}>
-                            <Card.Title>{currentUser.name}</Card.Title>
-                            <Card.Divider />
-                            <Text>{tip.tip}</Text>
-                            <Card.Divider />
-                            <View>
-                                <Button
-                                    title="Editar"
-                                    onPress={() =>
-                                        navigation.navigate("NewTip", {
-                                            game,
-                                            tip,
-                                        })
-                                    }
-                                />
-                                <Button
-                                    title="Apagar"
-                                    onPress={() => deleteTip(tip.id)}
-                                />
-                            </View>
-                        </Card>
-                    );
-                }
-            })}
+                    })}
+                </ScrollView>
+            </SafeAreaView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        marginHorizontal: 20,
+    },
+    commentButtonEdit: {
+        padding: 5,
+        backgroundColor: "orange",
+    },
+    commentButtonDelete: {
+        padding: 5,
+        backgroundColor: "red",
     },
 });
