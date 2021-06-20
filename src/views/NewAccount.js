d5b28a55ee393b27d7f86fe20899c9c9db555046
Matheus H. Potrich <matheushp.com@hotmail.com>
2021-06-20 18:42:57 -0300

    newAccount styles

--- src/views/NewAccount.js
+++ src/views/NewAccount.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
-import { Button } from "react-native-elements/dist/buttons/Button";
+import { Button } from "react-native-elements";
 
 import * as LoginAction from "../services/actions/loginAction";
 import * as UserAction from "../services/actions/userAction";
 import { useSelector, useDispatch } from "react-redux";
+import mainStyle from "../Styles/main";
 
 export default function NewAccount({ route, navigation }) {
     const dispatch = useDispatch();
@@ -38,62 +39,45 @@ export default function NewAccount({ route, navigation }) {
             <View style={{ marginLeft: 20, marginRight: 20 }}>
                 <Text>Nome:</Text>
                 <TextInput
-                    style={{
-                        width: "100%",
-                        padding: 10,
-                        borderColor: "gray",
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        marginTop: 10,
-                        marginBottom: 10,
-                    }}
+                    style={mainStyle.normalInput}
                     placeholder="nome"
                     value={name}
                     onChangeText={(text) => setName(text)}
                 />
                 <Text>Email:</Text>
                 <TextInput
-                    style={{
-                        width: "100%",
-                        padding: 10,
-                        borderColor: "gray",
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        marginTop: 10,
-                        marginBottom: 10,
-                    }}
+                    style={mainStyle.normalInput}
                     placeholder="Email"
                     value={email}
                     onChangeText={(text) => setEmail(text)}
                 />
                 <Text>Senha:</Text>
                 <TextInput
-                    style={{
-                        width: "100%",
-                        padding: 10,
-                        borderColor: "gray",
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        marginTop: 10,
-                        marginBottom: 10,
-                    }}
+                    style={mainStyle.normalInput}
                     placeholder="Senha"
                     value={password}
                     onChangeText={(text) => setPassword(text)}
                 />
                 <Button
                     title="Criar"
-                    buttonStyle={{
-                        backgroundColor: "green",
-                        borderRadius: 10,
-                        padding: 15,
-                    }}
+                    buttonStyle={mainStyle.greenButton}
                     loading={loading}
                     onPress={() => CreateUser()}
+                    disabled={
+                        name.length > 0 &&
+                        email.length > 0 &&
+                        password.length > 0
+                            ? false
+                            : true
+                    }
                 />
             </View>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 20,
+    },
+});
