31d858d8aa6e785a6aae96e3a1661d0873b95422
Matheus H. Potrich <matheushp.com@hotmail.com>
2021-06-20 14:41:20 -0300

    About information

--- src/views/About.js
+++ src/views/About.js
@@ -1,18 +1,53 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
+import { Text } from "react-native-elements";
 
 export default function About() {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
+            <SafeAreaView
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                <ScrollView>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>Nome: </Text>
+                        Matheus H. Potrich
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>Cursando: </Text>
+                        Ciências da Computação (5º semestre)
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>
+                            Instituição:{" "}
+                        </Text>
+                        IMED
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>Email: </Text>
+                        matheushp.com@gmail.com
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>RA: </Text>
+                        1117956
+                    </Text>
+                </ScrollView>
+            </SafeAreaView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: "center",
-        justifyContent: "center",
+        paddingTop: 20,
+        marginHorizontal: 20,
+    },
+    infoBox: {
+        marginBottom: 20,
     },
 });
