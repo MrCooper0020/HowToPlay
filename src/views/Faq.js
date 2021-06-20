a0f5a23b10d52730719c5b51e5e70b1287236f4b
Matheus H. Potrich <matheushp.com@hotmail.com>
2021-06-20 16:20:16 -0300

    populate Faq and HowWasMade

--- src/views/Faq.js
+++ src/views/Faq.js
@@ -1,18 +1,80 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
+import { Text } from "react-native-elements";
 
 export default function Faq() {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
+            <SafeAreaView
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                <ScrollView>
+                    <View style={{ alignItems: "center" }}>
+                        <Text h3 style={styles.title}>
+                            Funcionabilidades do app
+                        </Text>
+                    </View>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>Conta: </Text>É
+                        possivel criar uma conta no app e logar, o app tambem
+                        permite que email e senha ficam salvas para logar mais
+                        rapidamente.
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>
+                            Página de game:{" "}
+                        </Text>
+                        O app permite que usuarios que tenham uma conta e
+                        estejam logado, criem uma página sobre um jogo que o
+                        usuario queira discutir com a comunidade, tambem sendo
+                        possivel editar e deletar somente pelo usuario criador.
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>
+                            Comentarios:{" "}
+                        </Text>
+                        Dentro de uma pagina de jogo, é possivel que usuarios
+                        logados criem comentarios, tambem é possivel que os
+                        usuarios editem e apaguem os comentarios que eles
+                        criaram.
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>Dicas: </Text>
+                        Dentro de uma pagina de jogo, há um botão chamado "Sala
+                        de dicas" que está disponivel somente para usuarios
+                        logados, entrando nele, os usuario teram uma area
+                        somente para dicas sobre o jogo que podera ter spoiler
+                        sobre a historia do jogo em questao.
+                    </Text>
+                    <Text h4 style={styles.infoBox}>
+                        <Text style={{ fontWeight: "bold" }}>
+                            Minha localizacão:{" "}
+                        </Text>
+                        O app permite que os usuarios utilizem um mapa para nao
+                        se perderem na vida real.
+                    </Text>
+                </ScrollView>
+            </SafeAreaView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        paddingTop: 20,
+        marginHorizontal: 20,
+    },
+    infoBox: {
+        marginBottom: 20,
+    },
+    title: {
+        marginBottom: 30,
+        fontWeight: "bold",
         alignItems: "center",
-        justifyContent: "center",
     },
 });
--- src/views/HowWasMade.js
+++ src/views/HowWasMade.js
@@ -1,18 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
+import { Text } from "react-native-elements";
 
 export default function HowWasMade() {
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
+            <SafeAreaView
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                <ScrollView>
+                    <View style={{ alignItems: "center" }}>
+                        <Text h3 style={styles.title}>
+                            Expo ou CLI?
+                        </Text>
+                    </View>
+                    <Text h4 style={styles.infoBox}>
+                        Foi utilizado nesse projeto o Expo, pois
+                        eu(desenvolvedor) uso um Iphone e uso um computador com
+                        Windows, e utilizando o Expo é umas das únicas maneira
+                        de desenvolver sem ter um computador com MacOS, outro
+                        motivo foi a facilidade de iniciar um projeto e utilizar
+                        recursos sem a necessidade de configurações complexas
+                        como vincular as bibliotecas com o projeto.
+                    </Text>
+                </ScrollView>
+            </SafeAreaView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        paddingTop: 20,
+        marginHorizontal: 20,
+    },
+    infoBox: {
+        marginBottom: 20,
+    },
+    title: {
+        marginBottom: 30,
+        fontWeight: "bold",
         alignItems: "center",
-        justifyContent: "center",
     },
 });
